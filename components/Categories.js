58e689319f02852fb258f0fa9699e6a39c7f4c22
Nathan <wenjuncai97@gmail.com>
2022-09-09 23:20:02 -0400

    added categories component

--- components/Categories.js
+++ components/Categories.js
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { ScrollView, View, Text } from 'react-native'
 import React from 'react'
+import CategoryCard from './CategoryCard';
 
 const Categories = () => {
-  return (
-    <View>
-      <Text>Categories Hello</Text>
-    </View>
-  )
-}
+    return (
+        <ScrollView horizontal
+            contentContainerStyle={{
+                paddingHorizontal: 15,
+                paddingTop: 10,
+            }}
+            showsHorizontalScrollIndicator={false}>
+            { /* Category Card */}
+
+            <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing"/>
+            <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing"/>
+            <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing"/>
+        </ScrollView>
+    );
+};
 
 export default Categories;
